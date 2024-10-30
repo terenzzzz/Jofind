19c583a8ef7c0ce0556155ea1397c27f09ce8f22
Terenz <terence592592@gmail.com>
2024-10-30 16:22:25 +0800

    company edit

--- src/views/company/CompanyView.vue
+++ src/views/company/CompanyView.vue
@@ -3,7 +3,7 @@
     <div class="row mt-3">
       <div class="card p-5 border-0">
         <!--    公司基本信息-->
-        <div class="d-flex justify-content-between">
+        <div class="d-flex justify-content-between align-items-center">
           <div class="d-flex">
             <div
               class="img-container border border-2 p-2"
@@ -17,7 +17,7 @@
               <p class="text-muted">{{ jobs[0].company.location }}</p>
             </div>
           </div>
-          <p class="text-primary">Edit</p>
+          <p class="btn btn-outline-primary"  data-bs-toggle="modal" data-bs-target="#companyEditModal">Edit</p>
         </div>
         <div class="row py-2 gx-5 gy-3 mt-3">
           <div class="col-3 d-flex flex-column">
@@ -62,6 +62,79 @@
         </div>
         <el-divider />
 
+        <div class="modal fade" id="companyEditModal" tabindex="-1" >
+          <div class="modal-dialog modal-xl modal-dialog-centered">
+            <div class="modal-content card p-4">
+              <div class="d-flex justify-content-between">
+                <p class="btn"  data-bs-toggle="modal" data-bs-target="#companyEditModal"><i class="bi bi-x-lg"></i></p>
+                <p class="btn btn-primary" >Save</p>
+              </div>
+
+              <div class="d-flex mt-5 align-items-center">
+                <div class="img-container border border-2 p-2" style="height: 100px; width: 100px">
+                  <img :src="companyLogo" class="img-fluid" />
+                </div>
+                <div class="mb-3 ms-3">
+                  <label for="formFileSm" class="form-label">Upload Company Logo</label>
+                  <input class="form-control form-control-sm" id="formFileSm" type="file" @change="onFileChange">
+                </div>
+              </div>
+
+
+
+              <div class="row py-2 gx-5 gy-3 mt-3">
+                <div class=" col-12 d-flex flex-column">
+                  <p class="text-muted">Company Name</p>
+                  <input class="fw-bold" v-model="jobs[0].company.name">
+                </div>
+
+                <div class="col-3 d-flex flex-column">
+                  <p class="text-muted">Founded</p>
+                  <input v-model="jobs[0].company.founded">
+                </div>
+
+                <div class="col-3 d-flex flex-column">
+                  <p class="text-muted">Industry</p>
+                  <input v-model="jobs[0].company.industry">
+                </div>
+
+                <div class="col-3 d-flex flex-column">
+                  <p class="text-muted">Number of employee</p>
+                  <input v-model="jobs[0].company.size">
+                </div>
+
+                <div class="col-3 d-flex flex-column">
+                  <p class="text-muted">Website</p>
+                  <input v-model="jobs[0].company.url">
+                </div>
+
+                <div class="col-3 d-flex flex-column">
+                  <p class="text-muted">Location</p>
+                  <input v-model="jobs[0].company.location">
+                </div>
+
+                <div class="col-3 d-flex flex-column">
+                  <p class="text-muted">Latitude</p>
+                  <input v-model="jobs[0].company.latitude" type="number">
+<!--                  <p class="fw-bold">{{ jobs[0].company.latitude }}</p>-->
+                </div>
+
+                <div class="col-3 d-flex flex-column">
+                  <p class="text-muted">Longitude</p>
+                  <input v-model="jobs[0].company.longitude" type="number">
+                </div>
+
+                <div class="col-12 d-flex flex-column">
+                  <p class="text-muted">Company Background</p>
+                  <textarea v-model="jobs[0].company.background"/>
+<!--                  <p class="fw-bold">{{ jobs[0].company.background }}</p>-->
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
         <!--        状态信息-->
         <div class="row">
           <div class="col-4">
@@ -103,6 +176,18 @@ const outputValue = useTransition(source, {
   duration: 1000,
 })
 source.value = 10
+
+const companyLogo = ref(jobs[0].company.logo)
+
+function onFileChange(event) {
+  const file = event.target.files[0];
+  if (file) {
+    // 创建一个URL指向这个文件对象
+    companyLogo.value = URL.createObjectURL(file);
+  } else {
+    companyLogo.value = null;
+  }
+}
+
 </script>
 
-<style scoped lang="css"></style>
